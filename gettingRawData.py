58684ef1146da0f502bc236a3cf9908b57fa588b
snayyar00 <sidharth15nayyar@gmail.com>
2021-11-16 18:09:24 -0800

    removed print useless commit

--- gettingRawData.py
+++ gettingRawData.py
@@ -8,6 +8,5 @@ df_calgary = pd.read_csv(zf_calgary.open('calgary_crime.csv'))
 zf_toronto = zipfile.ZipFile('Toronto_crime.zip') 
 df_toronto = pd.read_csv(zf_toronto.open('Toronto_crime.csv'))
 
-print(df_toronto)
 
 
